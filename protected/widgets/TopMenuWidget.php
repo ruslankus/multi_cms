e8ae99c765fc1a62cd057d99a483661d3a15e92c
ruslankus <ruslankus@yahoo.com>
2014-06-24 00:57:00 +0300

    Finished language selection statement in widget

--- protected/widgets/TopMenuWidget.php
+++ protected/widgets/TopMenuWidget.php
@@ -3,12 +3,9 @@ class TopMenuWidget extends CWidget {
 
     public function run(){
         
-        $objNav = Navigation::model()->with(array('page_content.name',array('language' => 2)))->findAllByAttributes(array('type' => 1),array('order'=>'priority ASC'));
-        
-       
-               
-        $this->d($objNav);
-               
+        $objNav = Navigation::model()->with(array('page_content'=> array('on'=>'page_content.language = 4')))->findAllByAttributes(array('type' => 1),array('order'=>'priority ASC'));
+    
+    
         $this->render('top_menu',array('objNav' => $objNav));
     }    
     
